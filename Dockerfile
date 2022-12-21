b4c7bf30fd2d7f2d6f209fbc4cacedb68ca37b4c
mw <mw@1wilson.org>
2022-12-21 15:33:16 +0100

    update Dockerfile to work with kubernetes

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,7 @@ COPY ./requirements.txt /home/app/requirements.txt
 COPY ./pmml.py /home/app/pmml.py
 COPY ./entrypoint.sh /home/app/entrypoint.sh
 
+ENV POLL_INTERVAL=60
 RUN pip install -r /home/app/requirements.txt 
 
-ENTRYPOINT ["/home/app/entrypoint.sh"]
+CMD ["/home/app/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,11 @@
 DOCKER_USER=app
 DOCKER_HOME=/home/app
 
+cp /.fetchmailrc $DOCKER_HOME/.fetchmailrc
+cp /.pmmlrc $DOCKER_HOME/.pmmlrc
+
 chown app $DOCKER_HOME/.fetchmailrc
-chown app $DOCKER_HOME/pmml.py
 chown app $DOCKER_HOME/.pmmlrc
-exec runuser -u $DOCKER_USER -- fetchmail -d 60 -N
+chmod 700 $DOCKER_HOME/.fetchmailrc
+
+exec runuser -u $DOCKER_USER -- fetchmail -d $POLL_INTERVAL -N
